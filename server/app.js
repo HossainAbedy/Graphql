0483f2bb051ea3a4eb722a50226b1bee54495d74
Abedy <abedy.ewu@gmail.com>
2019-11-14 15:30:44 +0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+    
+}))
+
+app.listen(5000,() => {
+    console.log("Listening for requests in port 5000");
+});
